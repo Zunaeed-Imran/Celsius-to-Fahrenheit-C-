97fb1c649038a4f5fd7c0267817ace7c3135c425
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-31 10:01:08 +0600

    convert temperature in Celsius to Fahrenheit C++

---
+++ Celsius_toFahrenhite.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int C, F;
+
+  cout << "Enter The Celsius Number: ";
+  cin >> C;
+
+  F = C * 2 + 30;
+
+  cout << "The Celsius Number is: " << F << endl;
+
+  return 0;
+}
